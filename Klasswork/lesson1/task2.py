87497fba1d578f871811771d30a004ff3ed6d3a0
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-09-25 20:31:26 +0700

    add1i2

---
+++ Klasswork/lesson1/task2.py
@@ -0,0 +1,16 @@
+
+a = int(input("Введите A: "))
+b = int(input("Введите B: "))
+c = int(input("Введите C: "))
+d = int(input("Введите D: "))
+e = int(input("Введите E: "))
+max = a
+if b > max:
+    max = b
+if c > max:
+    max = c
+if d > max:
+    max = d
+if e > max:
+    max = e
+print(max)
