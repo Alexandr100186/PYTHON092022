1d12ed07e58667656b71127488f3f304d00b11d3
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-10-11 08:55:46 +0700

    addt1

--- Homework/lesson5/task1.py
+++ Homework/lesson5/task1.py
@@ -1,1 +1,15 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+import re
+
+s = "Напишите программу, удаляющую из текста все слова, содержащие абв автобус"
+words = re.split('\W+', s)
+print(words)
+lett = ["а", "б", "в"]
+
+for i in range(len(words)):
+    if words[i].find(lett[0]) != -1 and words[i].find(lett[1]) != -1 and words[i].find(lett[2]) != -1:
+        words[i] = None
+
+words = [txt for txt in words if txt]
+print(*words)
