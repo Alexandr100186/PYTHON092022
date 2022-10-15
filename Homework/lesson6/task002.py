be606abccc87433f3d90120951dee8b6ce44a8f7
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-10-15 21:45:37 +0700

    homeend

---
+++ Homework/lesson6/task002.py
@@ -0,0 +1,22 @@
+# 2. Дана последовательность чисел.
+# Получить список уникальных элементов заданной последовательности,
+# список повторяемых и убрать дубликаты из заданной последовательности.
+# Пример:
+# [1, 2, 3, 5, 1, 5, 3, 10] => [2, 10] и [1, 3, 5] и [1, 2, 5, 3, 10]
+
+from random import randint
+from typing import List
+
+def create_list(size, m, n):
+    return [randint(m, n) for i in range(size)]
+
+def get_unic_value(list):
+    return [i for i in set(list)]
+
+size = 10
+m = 1
+n = 10
+
+origin = create_list(size, m, n)
+print(origin)
+print(get_unic_value(origin))
